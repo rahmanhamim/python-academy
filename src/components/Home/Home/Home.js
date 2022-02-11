b7a02ac869cb5fadb79f04058364896de804ecbb
Hamim <rahmanhamiminfo@gmail.com>
2022-02-11 17:56:21 +0600

    carousel added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 import HomeCourses from "../HomeCourses/HomeCourses";
 import HomeFeature from "../HomeFeature/HomeFeature";
 import HomeResource from "../HomeResource/HomeResource";
+import HomeReview from "../HomeReview/HomeReview";
 import HomeTips from "../HomeTips/HomeTips";
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
    <HomeCourses />
    <HomeResource />
    <HomeTips />
+   <HomeReview />
   </>
  );
 };
