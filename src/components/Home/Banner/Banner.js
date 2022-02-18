8a6484fef4a97268409ea1722c726e3cb7f41f1c
Hamim <rahmanhamiminfo@gmail.com>
2022-02-18 17:44:31 +0600

    blogs

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -4,6 +4,7 @@ import Navigation from "../../Shared/Navigation/Navigation";
 import bgImg from "../../../img/banner-img.png";
 import { Box, Button, Container, Typography } from "@mui/material";
 import LoginFrom from "../LoginForm/LoginFrom";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
  const [open, setOpen] = React.useState(false);
@@ -24,6 +25,7 @@ const Banner = () => {
    minHeight: "60vh",
    display: "flex",
    justifyContent: "center",
+   alignItems: "start",
    flexDirection: "column",
   },
   bannerTitle: {
@@ -47,24 +49,31 @@ const Banner = () => {
       With a decade of insights and expertise, we're decided to <br /> reimagine
       co-working space.
      </Typography>
-     <Button
-      sx={{
-       bgcolor: "#FF4958",
-       color: "#fff",
-       maxWidth: "200px",
-       padding: "1.2em 0",
-       border: "1px solid #FF4958",
-       borderRadius: "3px",
-       fontFamily: "Manrope",
-       mb: { xs: 10, md: 0 },
-       "&:hover": {
-        bgcolor: "#F9B233",
-        border: "1px solid #F9B233",
-       },
+     <Link
+      to="/allcourses"
+      style={{
+       textDecoration: "none",
       }}
      >
-      Start Learning
-     </Button>
+      <Button
+       sx={{
+        bgcolor: "#FF4958",
+        color: "#fff",
+        maxWidth: "200px",
+        padding: "1.5em 2em",
+        border: "1px solid #FF4958",
+        borderRadius: "3px",
+        fontFamily: "Manrope",
+        mb: { xs: 10, md: 0 },
+        "&:hover": {
+         bgcolor: "#F9B233",
+         border: "1px solid #F9B233",
+        },
+       }}
+      >
+       Start Learning
+      </Button>
+     </Link>
     </Box>
     <LoginFrom open={open} handleClose={handleClose}></LoginFrom>
    </Container>
